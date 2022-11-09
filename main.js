30ee212bdc506dfdaf99bcd6c407b998c2b17e58
Ethan Swafford <ethanswafford@yahoo.com>
2022-11-08 19:16:17 -0800

    devPush

--- main.js
+++ main.js
@@ -10,6 +10,15 @@
 // Place the .join() method invocation as an argument of a console.log() statement to log the final story to the console.
 
 
+// Now it’s time to start editing the story by manipulating the storyWords array. We want to be able to see the changes, 
+// so be sure your console.log() of the joined story is the last line of code in your editor.
+// First, above the console.log() statement that uses the .join() method, create a variable named count that stores the number 0.
+// Directly below count, use a .forEach() method to iterate over the storyWords array. As an argument of the forEach() method, create an empty function to be used as the callback function.
+// While ES6 arrow syntax is recommended for the callback function, feel free to use any syntax you’re comfortable with.
+
+
+
+
 
 let story = 'Last weekend, I took literally the most beautifull bike ride of my life. The route is called "The 9W to Nyack" and it stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it literally took me an entire day. I stopped at Riverbank State Park to take some artsy photos. It was a short stop, though, because I had a freaking long way to go. After a quick photo op at the very popular Little Red Lighthouse I began my trek across the George Washington Bridge into New Jersey. The GW is a breathtaking 4,760 feet long! I was already very tired by the time I got to the other side. An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautifull park along the coast of the Hudson. Something that was very surprising to me was that near the end of the route you literally cross back into New York! At this point, you are very close to the end.';
 
@@ -20,6 +29,16 @@ let badWord = 'freaking';
 
 
 // console.log(storyWords);
+storyWords.forEach( () => {
+
+});
+
+
+
+
+
+
+
 console.log(storyWords.join(' '));
 
 
